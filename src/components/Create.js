f095d1a685ed81675678d22e7f4b6c4bb68218f2
Sandhya Thiruthamarai Ramaswamy <61589919+sandhyavenky@users.noreply.github.com>
2020-05-15 22:19:22 -0230

    Update Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -7,6 +7,13 @@ import ReactDom from 'react-dom'
 window.render = () => {
   ReactDom.render(<Create/>, document.getElementById('root'));
 }
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => (valid = false)
+  );
+  return valid;
+}
 class Create extends React.Component{
   constructor(props){
     super(props);
@@ -21,7 +28,15 @@ class Create extends React.Component{
         dob: '',
         gender: ''
       }  , employeeStatus: "" ,
-      errormessage: '',  
+      errormessage: '', 
+      errors: {
+        empid: '',
+        fname: '',
+        sname: '',
+        email: '',
+        dob: '',
+        gender: ''
+      } 
     };
   }
   
@@ -29,29 +44,41 @@ createEmployee(event){
   const name = event.target.name;
   const value = event.target.value;
   let err = ''; 
+  let errors = this.state.errors;
   var employee = Object.assign(this.state.emp, {[event.target.name]:  event.target.value} ) 
   if ((name === "empid"),(name === "dob") ){
-    if(value != "" && !Number(value)) {
+    if(value !== "" && !Number(value)) {
       err = <strong> Please enter numeric values for Employee ID and Data of Birth</strong>;
     }
   }
+  this.setState({errors, [name]: value} , () => {
+    console.log(errors)
+  })
   this.setState({errormessage: err});
   this.setState({
     emp: employee           
   }); 
 }
-submitDetails(){
-  axios.post("http://localhost:3001/", this.state.emp)  
+submitDetails(e){
+  e.preventDefault();
+  if(validateForm(this.state.errors)){
+    axios.post("http://localhost:3001/", this.state.emp)  
     .then(res => {
       this.setState({
         employeeStatus: res.data.status 
       })
     })
+  }
+  else{
+    alert("Invalid form. Enter valid Details")
+  }
 }
+
 clearDetails = () => {   
        window.render()
 }
 render(){
+  const {errors} = this.state;
   return(
     <div>
       {/* <Container fluid="sm" style={{ paddingTop: 30, paddingBottom: 30}} > */}
@@ -75,30 +102,30 @@ render(){
       <hr color="lightblue"></hr>
       <Form.Group><Form.Row>
         <Col xs={2}>Employee ID</Col>
-        <Col xs={3}> <Form.Control type="text" name="empid" value={this.state.emp.empid} onChange={this.createEmployee}/></Col>
+        <Col xs={3}> <Form.Control type="text" name="empid" value={this.state.emp.empid} onChange={this.createEmployee} noValidate /></Col>
       </Form.Row></Form.Group>
       <Form.Group><Form.Row>
         <Col xs={2}>First Name</Col>
-        <Col xs={3}> <Form.Control type="text" name="fname" value={this.state.emp.fname} onChange={this.createEmployee}/></Col>
+        <Col xs={3}> <Form.Control type="text" name="fname" value={this.state.emp.fname} onChange={this.createEmployee} noValidate /></Col>
       </Form.Row></Form.Group>
       <Form.Group><Form.Row>
         <Col xs={2}>Sur Name</Col>
-        <Col xs={3}> <Form.Control type="text" name="sname" value={this.state.emp.sname} onChange={this.createEmployee}/></Col>
+        <Col xs={3}> <Form.Control type="text" name="sname" value={this.state.emp.sname} onChange={this.createEmployee} noValidate /></Col>
       </Form.Row></Form.Group>
       <Form.Group><Form.Row>
         <Col xs={2}>Email</Col>
-        <Col xs={3}> <Form.Control type="text" name="email" value={this.state.emp.email} onChange={this.createEmployee}/></Col>
+        <Col xs={3}> <Form.Control type="text" name="email" value={this.state.emp.email} onChange={this.createEmployee} noValidate /></Col>
         </Form.Row></Form.Group>
       </Form>
       <Form>
       <Form.Group><Form.Row>
         <Col xs={2}>DOB</Col>
-        <Col xs={3}> <Form.Control type="text" name="dob" value={this.state.emp.dob} onChange={this.createEmployee} /></Col>
+        <Col xs={3}> <Form.Control type="text" name="dob" value={this.state.emp.dob} onChange={this.createEmployee} noValidate /></Col>
         </Form.Row></Form.Group>
         <Form.Group><Form.Row>
           <Col xs={2}> Gender </Col>
-          <Col xs={1}><Form.Check type="radio" name="gender" value="Male" onChange={this.createEmployee} checked={this.state.emp.gender === "Male"} label="Male" ></Form.Check></Col>
-          <Col xs={1}><Form.Check type="radio" name="gender" value="Female" onChange={this.createEmployee} checked={this.state.emp.gender === "Female"} label="Female"></Form.Check></Col>
+          <Col xs={1}><Form.Check type="radio" name="gender" value="Male" onChange={this.createEmployee} noValidate checked={this.state.emp.gender === "Male"} label="Male" ></Form.Check></Col>
+          <Col xs={1}><Form.Check type="radio" name="gender" value="Female" onChange={this.createEmployee} noValidate checked={this.state.emp.gender === "Female"} label="Female"></Form.Check></Col>
         </Form.Row></Form.Group>
         <Form.Group><Form.Row>
           <Col xs={1}><Button variant="primary" size="sm" onClick={this.submitDetails}> Create </Button></Col>
